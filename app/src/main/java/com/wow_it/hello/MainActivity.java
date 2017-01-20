bc76491236f05d86b06572797c4f302b7f1a856a
Noor Mohammed Anik <noormohammedanik@gmail.com>
2017-01-20 18:42:54 +0600

    Update on UI

--- app/src/main/java/com/wow_it/hello/MainActivity.java
+++ app/src/main/java/com/wow_it/hello/MainActivity.java
@@ -1,13 +1,18 @@
 package com.wow_it.hello;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView=(TextView)findViewById(R.id.textView);
+
     }
 }
